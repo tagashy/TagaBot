83956662093b95a4f5edb87c8cdfaa6a22375b87
Tagashy <baboin.arthur@gmail.com>
2017-01-06 15:37:01 +0100

    updating Transfer class to work with new architecture

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ import RandomQuote
 import transfert_class
 import TagaBot
 cr=controller.Controller()
-reg=Message(pseudo="ghozt", msg_type="PRIVMSG")
+reg=Message(target="#test")
 cr.start()
-bot=TagaBot.Bot(cr,)#RandomQuote.Troll(cr,"loulou","ghozt","irc.root-me.org","#root-me-bot")
+bot=transfert_class.Transferrer(cr,"#root-me-bot","loulou","irc.root-me.org")#TagaBot.Bot(cr,)#RandomQuote.Troll(cr,"loulou","ghozt","irc.root-me.org","#root-me-bot")
 cr.add_bot(reg,bot,channel="#root-me-bot",server="irc.root-me.org",bot_name="loulou")#,sock=sock)#channel="#root-me-bot",server="irc.root-me.org",bot_name="test")
-cr.add_bot_listener()
+cr.join_channel("irc.root-me.org","loulou","#test")
--- transfert_class.py
+++ transfert_class.py
@@ -10,12 +10,18 @@ class Transferrer(IRC.Bot):
     Transfer class, it will transfer all message from a channel to another
     """
 
-    def __init__(self, parent, send_sock, original_chan, pseudo=None, couleur=2):
-        IRC.Bot.__init__(self, parent)
-        self.pseudo = pseudo
-        self.send_sock = send_sock
+    def __init__(self, parent, target,bot_name,server_to_reply, couleur=2):
+        """
+        initialisation of Transfer class
+
+        :param parent: the dispatcher
+        :param target: the target to reply to (channel or username)
+        :param bot_name: the name of the bot
+        :param server_to_reply: the server to reply to
+        :param couleur: the color of the message to write
+        """
+        IRC.Bot.__init__(self, parent,target,bot_name,server_to_reply)
         self.couleur = couleur
-        self.original_chan = original_chan
         self.invisible_cara = u"\u200B"
 
     def send_message(self, message):
@@ -40,7 +46,8 @@ class Transferrer(IRC.Bot):
         self.add_user(message)
         send_res = "User {} has join channel {}".format(message.pseudo[0:1] + self.invisible_cara + message.pseudo[1:],
                                                         message.target)
-        self.send_message(send_res)
+        # self.send_message(send_res)
+        self.reply(send_res, "PRIVMSG")
 
     def user_privmsg(self, message):
         """
@@ -53,7 +60,8 @@ class Transferrer(IRC.Bot):
         send_res = "Private Message from user {}>{}".format(
             message.pseudo[0:1] + self.invisible_cara + message.pseudo[1:],
             message.content)
-        self.send_message(send_res)
+        #self.send_message(send_res)
+        self.reply(send_res, "PRIVMSG")
 
     def user_pubmsg(self, message):
         """
@@ -65,7 +73,8 @@ class Transferrer(IRC.Bot):
         self.update_user_last_seen(message)
         send_res = "{} : {}>{}".format(message.target, message.pseudo[0:1] + self.invisible_cara + message.pseudo[1:],
                                        message.content)
-        self.send_message(send_res)
+        #self.send_message(send_res)
+        self.reply(send_res,"PRIVMSG")
 
     def user_quit(self, message):
         """
@@ -77,7 +86,8 @@ class Transferrer(IRC.Bot):
         self.deactivate_user(message)
         send_res = "User {} has quit server with msg : {}".format(
             message.pseudo[0:1] + self.invisible_cara + message.pseudo[1:], message.content)
-        self.send_message(send_res)
+        # self.send_message(send_res)
+        self.reply(send_res, "PRIVMSG")
 
     def user_part(self, message):
         """
@@ -89,7 +99,8 @@ class Transferrer(IRC.Bot):
         self.deactivate_user(message)
         send_res = "User {} has quit channel {} with msg : {}".format(
             message.pseudo[0:1] + self.invisible_cara + message.pseudo[1:], message.target, message.content)
-        self.send_message(send_res)
+        # self.send_message(send_res)
+        self.reply(send_res, "PRIVMSG")
 
     def user_ban(self, message):
         """
@@ -102,7 +113,8 @@ class Transferrer(IRC.Bot):
         send_res = "User {} has been banned from channel {} by {}".format(message.content[0:1] + self.invisible_cara +
                                                                           message.content[1:], message.target,
                                                                           message.pseudo)
-        self.send_message(send_res)
+        # self.send_message(send_res)
+        self.reply(send_res, "PRIVMSG")
 
     def user_kick(self, message):
         """
@@ -115,4 +127,5 @@ class Transferrer(IRC.Bot):
         send_res = "User {} has been kicked from channel {} by {}".format(message.content[0:1] + self.invisible_cara +
                                                                           message.content[1:], message.target,
                                                                           message.pseudo)
-        self.send_message(send_res)
+        # self.send_message(send_res)
+        self.reply(send_res, "PRIVMSG")
